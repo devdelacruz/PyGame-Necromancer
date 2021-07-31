8c4ce55f8095c09cc75c9b66abeb40c72434b50b
devdelacruz <78395652+devdelacruz@users.noreply.github.com>
2021-07-31 21:04:34 +0800

    gameVersion = "v0.2.1"

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from Events import Game
 import pygame
 
 g = Game()
-
+# gameVersion = "v0.2.1"
 pygame.mixer.music.load("assets/OST/Warhammer 40,000 Mechanicus Soundtrack - 10. Noosphere.mp3")
 pygame.mixer.music.play(-1)
 
